d23f0e896660ecfe7ba680a720e2f018762bdc51
Gregory <GregoryCB@outlook.com>
2018-02-06 22:39:16 +0000

    smoother backing

--- gbPyScripts/FileMenu.py
+++ gbPyScripts/FileMenu.py
@@ -83,7 +83,7 @@ def FileBrowser(stdscr):
     stdscr.clear()
     curses.init_pair(1,curses.COLOR_BLACK, curses.COLOR_WHITE)
     menu = OptionMenu(2,2,10,15)
-    stack = [['/Users/Greg/Dropbox',0,0]]
+    stack = [['/Users/Greg/Dropbox',0,0,'']]
     changedDir = True
     result = ''
     done = False
@@ -93,9 +93,13 @@ def FileBrowser(stdscr):
         menu.setSize(1,1,h-2,w-2)
         cwd = stack[-1][0]
         if changedDir:
+            search = ''
             options = os.listdir(cwd)
             options = [(x+'/' if os.path.isdir(os.path.join(cwd,x)) else x) for x in options]
             menu.setOptions(options)
+            if len(stack[-1][3]) > 0:
+                search = stack[-1][3]
+                menu.addSort((lambda x: fuzz.partial_ratio(x.lower(),search.lower())))
             menu.setView(stack[-1][1], stack[-1][2])
             changedDir = False
         menu.draw(stdscr)
@@ -113,8 +117,9 @@ def FileBrowser(stdscr):
         elif c == curses.KEY_RIGHT:
             selection = menu.getSelection()
             if os.path.isdir(os.path.join(cwd,selection)):
-                stack[-1][1], stack[-1][2] = menu.getView()     
-                stack.append([os.path.join(cwd,selection),0,0])
+                stack[-1][1], stack[-1][2] = menu.getView()
+                stack[-1][3] = search     
+                stack.append([os.path.join(cwd,selection),0,0,''])
                 changedDir = True
         elif c == curses.KEY_LEFT and len(stack) > 1:
             stack = stack[:-1]
@@ -122,12 +127,12 @@ def FileBrowser(stdscr):
         elif c == curses.KEY_BACKSPACE or c == 127: 
             if len(search) > 0:
                 search = search[:-1]
-                menu.addSort((lambda x: fuzz.partial_ratio(x.lower(),search.lower()) == 100))
+                menu.addSort((lambda x: fuzz.partial_ratio(x.lower(),search.lower())))
             else:
                 menu.removeSort()
         elif curses.ascii.isascii(c):
             search += chr(c)
-            menu.addSort((lambda x: fuzz.partial_ratio(x.lower(),search.lower()) == 100))
+            menu.addSort((lambda x: fuzz.partial_ratio(x.lower(),search.lower())))
     return result 
 
 if __name__ == '__main__':
